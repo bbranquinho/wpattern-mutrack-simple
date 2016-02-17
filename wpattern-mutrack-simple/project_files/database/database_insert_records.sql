dabe14c4d4a37c951aa7210141dd1e88f595c689
Augusto Afonso Borges Branquinho <augustobranquinho@gmail.com>
2016-02-17 01:37:54 -0200

    Fix the warning message.

--- wpattern-mutrack-simple/project_files/database/database_insert_records.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `mutrack_simple`.`tb_user` (`id`, `name`, `email`, `password`) VALUES ('1', 'admin', 'admin@admin.com', 'admin');
-INSERT INTO `mutrack_simple`.`tb_user` (`id`, `name`, `email`, `password`) VALUES ('2', 'user', 'user@user.com', 'user');
-
-INSERT INTO `mutrack_simple`.`tb_user_permission` (`permission_id`, `user_id`) VALUES ('1', '1');
-INSERT INTO `mutrack_simple`.`tb_user_permission` (`permission_id`, `user_id`) VALUES ('2', '1');
-INSERT INTO `mutrack_simple`.`tb_user_permission` (`permission_id`, `user_id`) VALUES ('2', '2');
