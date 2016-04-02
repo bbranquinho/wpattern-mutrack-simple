94fdee6cf99b392dfc6c6f6c7d0f63697e975afa
Augusto Afonso Borges Branquinho <augustobranquinho@gmail.com>
2016-04-02 16:32:25 -0300

    Developing the web.

---
+++ wpattern-mutrack-simple-web/app/src/packagee/packagee.js
@@ -0,0 +1,66 @@
+'use strict';
+
+angular.module('mutrack')
+  .controller('PackageeCtrl', function($scope, ngNotify, RestSrv, SERVICE_PATH) {
+    $scope.packagee = {};
+    $scope.packagees = [];
+    $scope.showAddEditPackagee = false;
+
+    ngNotify.config({
+      theme: 'pastel'
+    });
+
+    // Show the form used to edit or add packagees.
+    $scope.show = function() {
+      $scope.showAddEditPackagee = true;
+    };
+
+    // Hide the form used to edit or add packagees.
+    $scope.hide = function() {
+      $scope.showAddEditPackagee = false;
+      $scope.packagee = {};
+    };
+
+    // Manage CRUD operations.
+    var packageeUrl = SERVICE_PATH.PRIVATE_PATH + '/packagee';
+
+    $scope.editPackagee = function(packagee) {
+      $scope.packagee = angular.copy(packagee);
+      $scope.show();
+    };
+
+    $scope.deletePackagee = function(packagee) {
+      RestSrv.delete(packageeUrl, packagee, function() {
+        $scope.packagees.splice($scope.packagees.indexOf(packagee), 1);
+        ngNotify.set('Packagee \'' + packagee.name + '\' deleted.', 'success');
+      });
+    };
+
+    $scope.savePackagee = function(packagee) {
+      if (packagee.id) {
+        RestSrv.edit(packageeUrl, packagee, function() {
+          for (var i = 0; i < $scope.packagees.length; i++) {
+            if ($scope.packagees[i].id === packagee.id) {
+              $scope.packagees[i] = packagee;
+            }
+          }
+
+          $scope.hide();
+          ngNotify.set('Packagee \'' + packagee.name + '\' updated.', 'success');
+        });
+      } else {
+        RestSrv.add(packageeUrl, packagee, function(newPackagee) {
+          $scope.packagees.push(newPackagee);
+          $scope.hide();
+          ngNotify.set('Packagee \'' + packagee.name + '\' added.', 'success');
+        });
+      }
+    };
+
+    // Request all data (packagees).
+    RestSrv.find(packageeUrl, function(data) {
+      $scope.packagees = data;
+      ngNotify.set('Packagees loaded with succeess.', 'success');
+    });
+
+  });
