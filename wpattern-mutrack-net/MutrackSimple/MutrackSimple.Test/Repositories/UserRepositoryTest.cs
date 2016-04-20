e7cca761fe1adb4d01f2839d7d14f054b6174852
Augusto Afonso Borges Branquinho <augustobranquinho@gmail.com>
2016-04-19 23:40:52 -0300

    Create a test project.

---
+++ wpattern-mutrack-net/MutrackSimple/MutrackSimple.Test/Repositories/UserRepositoryTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MutrackSimple.Test.Repositories
+{
+    [TestClass]
+    public class UserRepositoryTest
+    {
+        [TestMethod]
+        public void TestGetAll()
+        {
+        }
+    }
+}
