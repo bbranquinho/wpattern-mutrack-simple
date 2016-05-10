5b10b97af6b044593ad0424f6fec487b5d0ba756
Augusto Afonso Borges Branquinho <augustobranquinho@gmail.com>
2016-05-10 17:19:55 -0300

    Developing new .NET services using WebAPI.

---
+++ wpattern-mutrack-net/MutrackSimple/MutrackSimple/Controllers/Utils/GenericController.cs
@@ -0,0 +1,15 @@
+﻿using MutrackSimple.Models.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MutrackSimple.Controllers.Utils
+{
+    public interface GenericController<T, PK> where T : BaseEntity<PK>
+    {
+        // GET: api/T
+        IQueryable<T> Get();
+    }
+}
---
+++ wpattern-mutrack-net/MutrackSimple/MutrackSimple/Controllers/Utils/GenericControllerImpl.cs
@@ -0,0 +1,48 @@
+﻿using MutrackSimple.Models;
+using MutrackSimple.Models.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace MutrackSimple.Controllers.Utils
+{
+    public abstract class GenericControllerImpl<T, PK> : ApiController, GenericController<T, PK> where T : BaseEntity<PK>
+    {
+        #region Properties
+        protected GenericRepository<T, PK> Repository { get; set; }
+
+        private Type asdd = typeof(PackageEntity);
+        #endregion
+
+        #region Constructors
+        public GenericControllerImpl(GenericRepository<T, PK> repository)
+        {
+            Repository = repository;
+        }
+        #endregion
+
+        #region Service Methods
+        // GET: api/T
+        public IQueryable<T> Get()
+        {
+            return Repository.GetQuery();
+        }
+
+        // GET: api/T/id
+        public IHttpActionResult Get(PK id)
+        {
+            T entity = Repository.Single(id);
+
+            if (entity == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(entity);
+        }
+        #endregion
+    }
+}
